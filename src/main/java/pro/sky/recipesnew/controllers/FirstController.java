72523c7f3bf2d4265121c1d59cc31fd102cc41cd
Olgadani <olga.metizzz@yandex.ru>
2023-01-02 00:08:02 +0400

    initial message

--- src/main/java/pro/sky/recipesnew/controllers/FirstController.java
+++ src/main/java/pro/sky/recipesnew/controllers/FirstController.java
@@ -1,4 +1,19 @@
 package pro.sky.recipesnew.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FirstController {
+    @GetMapping("/")
+    public String appWork() {
+        return "Приложение запущено";
+    }
+    @GetMapping("/info")
+    public String page() {
+        return "Данилова Ольга"
+                + "рецепты"
+                + "дата: 01.01.2023"
+                + "сайт с рецептами";
+    }
 }
